4c93d629da7d987829992f4ce3d369bfbee40062
Bic <victor.timosencho@gmail.com>
2025-12-27 12:51:50 -0300

    feat: configuracao de conexao dbsession e app.config

---
+++ TorinosERP.Infra.Data/Context/DbSession.cs
@@ -0,0 +1,32 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace TorinosERP.Infra.Data.Context
+{
+    public class DbSession : IDisposable
+    {
+        public IDbConnection Connection { get; }
+        public IDbTransaction Transaction { get; set; }
+
+        public DbSession()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["TorinosERP"].ConnectionString;
+
+            Connection = new NpgsqlConnection(connectionString);
+            Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            Transaction?.Dispose();
+            Connection?.Close();
+            Connection?.Dispose();
+        }
+    }
+}
