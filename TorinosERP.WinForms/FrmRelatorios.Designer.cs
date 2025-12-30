24a43e50f2083c07f63faae39bbf614f53baab26
Bic <victor.timosencho@gmail.com>
2025-12-29 22:23:30 -0300

    feat Implementação parcial da tela de relatórios relatório de vendas resumido)

--- TorinosERP.WinForms/FrmRelatorios.Designer.cs
+++ TorinosERP.WinForms/FrmRelatorios.Designer.cs
@@ -29,19 +29,234 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRelatorios));
+            lblStatusVenda = new Label();
+            cmbStatusVenda = new ComboBox();
+            grTipoData = new GroupBox();
+            rdbDataCadastro = new RadioButton();
+            rdbDataVenda = new RadioButton();
+            label1 = new Label();
+            lblDtInicial = new Label();
+            dtFinal = new DateTimePicker();
+            dtInicial = new DateTimePicker();
+            lblCliente = new Label();
+            cmbCliente = new ComboBox();
+            btnPesquisar = new Button();
+            pnlReportViewer = new Panel();
+            grTipoRelatorio = new GroupBox();
+            rdbAnalitico = new RadioButton();
+            rdbResumido = new RadioButton();
+            grTipoData.SuspendLayout();
+            grTipoRelatorio.SuspendLayout();
             SuspendLayout();
+            // 
+            // lblStatusVenda
+            // 
+            lblStatusVenda.AutoSize = true;
+            lblStatusVenda.Location = new Point(20, 79);
+            lblStatusVenda.Name = "lblStatusVenda";
+            lblStatusVenda.Size = new Size(39, 15);
+            lblStatusVenda.TabIndex = 46;
+            lblStatusVenda.Text = "Status";
+            // 
+            // cmbStatusVenda
+            // 
+            cmbStatusVenda.FormattingEnabled = true;
+            cmbStatusVenda.Location = new Point(20, 97);
+            cmbStatusVenda.Name = "cmbStatusVenda";
+            cmbStatusVenda.Size = new Size(221, 23);
+            cmbStatusVenda.TabIndex = 42;
+            // 
+            // grTipoData
+            // 
+            grTipoData.Controls.Add(rdbDataCadastro);
+            grTipoData.Controls.Add(rdbDataVenda);
+            grTipoData.Location = new Point(470, 33);
+            grTipoData.Name = "grTipoData";
+            grTipoData.Size = new Size(109, 87);
+            grTipoData.TabIndex = 45;
+            grTipoData.TabStop = false;
+            grTipoData.Text = "Filtro Data";
+            // 
+            // rdbDataCadastro
+            // 
+            rdbDataCadastro.AutoSize = true;
+            rdbDataCadastro.Enabled = false;
+            rdbDataCadastro.Location = new Point(6, 52);
+            rdbDataCadastro.Name = "rdbDataCadastro";
+            rdbDataCadastro.Size = new Size(99, 19);
+            rdbDataCadastro.TabIndex = 34;
+            rdbDataCadastro.Text = "Data Cadastro";
+            rdbDataCadastro.UseVisualStyleBackColor = true;
+            // 
+            // rdbDataVenda
+            // 
+            rdbDataVenda.AutoSize = true;
+            rdbDataVenda.Checked = true;
+            rdbDataVenda.Location = new Point(6, 27);
+            rdbDataVenda.Name = "rdbDataVenda";
+            rdbDataVenda.Size = new Size(84, 19);
+            rdbDataVenda.TabIndex = 35;
+            rdbDataVenda.TabStop = true;
+            rdbDataVenda.Text = "Data Venda";
+            rdbDataVenda.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(362, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 44;
+            label1.Text = "Data Final";
+            // 
+            // lblDtInicial
+            // 
+            lblDtInicial.AutoSize = true;
+            lblDtInicial.Location = new Point(258, 33);
+            lblDtInicial.Name = "lblDtInicial";
+            lblDtInicial.Size = new Size(65, 15);
+            lblDtInicial.TabIndex = 43;
+            lblDtInicial.Text = "Data Inicial";
+            // 
+            // dtFinal
+            // 
+            dtFinal.Format = DateTimePickerFormat.Custom;
+            dtFinal.Location = new Point(362, 51);
+            dtFinal.Name = "dtFinal";
+            dtFinal.Size = new Size(98, 23);
+            dtFinal.TabIndex = 41;
+            // 
+            // dtInicial
+            // 
+            dtInicial.Format = DateTimePickerFormat.Custom;
+            dtInicial.Location = new Point(258, 51);
+            dtInicial.Name = "dtInicial";
+            dtInicial.Size = new Size(98, 23);
+            dtInicial.TabIndex = 39;
+            // 
+            // lblCliente
+            // 
+            lblCliente.AutoSize = true;
+            lblCliente.Location = new Point(20, 33);
+            lblCliente.Name = "lblCliente";
+            lblCliente.Size = new Size(44, 15);
+            lblCliente.TabIndex = 40;
+            lblCliente.Text = "Cliente";
+            // 
+            // cmbCliente
+            // 
+            cmbCliente.FormattingEnabled = true;
+            cmbCliente.Location = new Point(20, 51);
+            cmbCliente.Name = "cmbCliente";
+            cmbCliente.Size = new Size(221, 23);
+            cmbCliente.TabIndex = 38;
+            // 
+            // btnPesquisar
+            // 
+            btnPesquisar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnPesquisar.BackColor = SystemColors.Window;
+            btnPesquisar.FlatStyle = FlatStyle.Flat;
+            btnPesquisar.Image = Properties.Resources.search_32dp_204058;
+            btnPesquisar.Location = new Point(704, 30);
+            btnPesquisar.Name = "btnPesquisar";
+            btnPesquisar.Size = new Size(69, 62);
+            btnPesquisar.TabIndex = 47;
+            btnPesquisar.Text = "Pesquisar";
+            btnPesquisar.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnPesquisar.UseVisualStyleBackColor = false;
+            btnPesquisar.Click += btnPesquisar_Click;
+            // 
+            // pnlReportViewer
+            // 
+            pnlReportViewer.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlReportViewer.Location = new Point(20, 142);
+            pnlReportViewer.Name = "pnlReportViewer";
+            pnlReportViewer.Size = new Size(753, 394);
+            pnlReportViewer.TabIndex = 48;
+            // 
+            // grTipoRelatorio
+            // 
+            grTipoRelatorio.Controls.Add(rdbAnalitico);
+            grTipoRelatorio.Controls.Add(rdbResumido);
+            grTipoRelatorio.Location = new Point(258, 85);
+            grTipoRelatorio.Name = "grTipoRelatorio";
+            grTipoRelatorio.Size = new Size(202, 51);
+            grTipoRelatorio.TabIndex = 49;
+            grTipoRelatorio.TabStop = false;
+            grTipoRelatorio.Text = "Tipo";
+            // 
+            // rdbAnalitico
+            // 
+            rdbAnalitico.AutoSize = true;
+            rdbAnalitico.Checked = true;
+            rdbAnalitico.Location = new Point(98, 22);
+            rdbAnalitico.Name = "rdbAnalitico";
+            rdbAnalitico.Size = new Size(72, 19);
+            rdbAnalitico.TabIndex = 37;
+            rdbAnalitico.TabStop = true;
+            rdbAnalitico.Text = "Analítico";
+            rdbAnalitico.UseVisualStyleBackColor = true;
+            // 
+            // rdbResumido
+            // 
+            rdbResumido.AutoSize = true;
+            rdbResumido.Checked = true;
+            rdbResumido.Location = new Point(12, 22);
+            rdbResumido.Name = "rdbResumido";
+            rdbResumido.Size = new Size(78, 19);
+            rdbResumido.TabIndex = 36;
+            rdbResumido.TabStop = true;
+            rdbResumido.Text = "Resumido";
+            rdbResumido.UseVisualStyleBackColor = true;
             // 
             // FrmRelatorios
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(797, 558);
+            Controls.Add(grTipoRelatorio);
+            Controls.Add(pnlReportViewer);
+            Controls.Add(btnPesquisar);
+            Controls.Add(lblStatusVenda);
+            Controls.Add(cmbStatusVenda);
+            Controls.Add(grTipoData);
+            Controls.Add(label1);
+            Controls.Add(lblDtInicial);
+            Controls.Add(dtFinal);
+            Controls.Add(dtInicial);
+            Controls.Add(lblCliente);
+            Controls.Add(cmbCliente);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MaximumSize = new Size(813, 597);
             Name = "FrmRelatorios";
             Text = "Relatórios";
+            Load += FrmRelatorios_Load;
+            grTipoData.ResumeLayout(false);
+            grTipoData.PerformLayout();
+            grTipoRelatorio.ResumeLayout(false);
+            grTipoRelatorio.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblStatusVenda;
+        private ComboBox cmbStatusVenda;
+        private GroupBox grTipoData;
+        private RadioButton rdbDataCadastro;
+        private RadioButton rdbDataVenda;
+        private Label label1;
+        private Label lblDtInicial;
+        private DateTimePicker dtFinal;
+        private DateTimePicker dtInicial;
+        private Label lblCliente;
+        private ComboBox cmbCliente;
+        private Button btnPesquisar;
+        private Panel pnlReportViewer;
+        private GroupBox grTipoRelatorio;
+        private RadioButton rdbAnalitico;
+        private RadioButton rdbResumido;
     }
 }
--- TorinosERP.WinForms/FrmRelatorios.cs
+++ TorinosERP.WinForms/FrmRelatorios.cs
@@ -1,20 +1,148 @@
-﻿using System;
+﻿using Microsoft.Reporting.WinForms;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TorinosERP.Domain.Enums;
+using TorinosERP.Domain.Interfaces.Repositories;
 
 namespace TorinosERP.WinForms
 {
     public partial class FrmRelatorios : Form
-    {
-        public FrmRelatorios()
+    {        
+        private ReportViewer reportViewer1;
+        
+        private readonly IVendaRepository _vendaRepo;
+        private readonly IClienteRepository _clienteRepo;
+        
+        public FrmRelatorios(IVendaRepository vendaRepo, IClienteRepository clienteRepo)
         {
             InitializeComponent();
+            _vendaRepo = vendaRepo;
+            _clienteRepo = clienteRepo;
+            
+            this.AcceptButton = btnPesquisar;
+        }
+       
+        private void FrmRelatorios_Load(object sender, EventArgs e)
+        {
+            ConfigurarReportViewer();
+            CarregarCombos();
+            
+            dtInicial.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            dtFinal.Value = DateTime.Now;
+            
+            rdbDataVenda.Checked = true;
+        }
+
+        private void ConfigurarReportViewer()
+        {
+            this.reportViewer1 = new ReportViewer();            
+            this.reportViewer1.Dock = DockStyle.Fill;
+            this.reportViewer1.ShowToolBar = true;
+            this.reportViewer1.ShowPageNavigationControls = true;
+            this.reportViewer1.ShowZoomControl = true;
+            
+            this.reportViewer1.ProcessingMode = ProcessingMode.Local;            
+            this.pnlReportViewer.Controls.Add(this.reportViewer1);
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private async void CarregarCombos()
+        {
+            try
+            {
+                var clientes = await _clienteRepo.ObterTodosAsync();
+                var listaClientes = new List<dynamic>
+                {
+                    new { Id = -1, Nome = "Todos" }
+                };
+
+                foreach (var c in clientes)
+                {
+                    listaClientes.Add(new { Id = c.Id, Nome = c.Nome });
+                }
+
+                cmbCliente.DataSource = listaClientes;
+                cmbCliente.DisplayMember = "Nome";
+                cmbCliente.ValueMember = "Id";
+
+                var listaStatus = new List<dynamic>
+                {
+                    new { Id = -1, Nome = "Todos" }
+                };
+
+                foreach (VendaStatus status in Enum.GetValues(typeof(VendaStatus)))
+                {
+                    listaStatus.Add(new { Id = (int)status, Nome = status.ToString() });
+                }
+
+                cmbStatusVenda.DataSource = listaStatus;
+                cmbStatusVenda.DisplayMember = "Nome";
+                cmbStatusVenda.ValueMember = "Id";
+                cmbStatusVenda.SelectedValue = (int)VendaStatus.Efetivada;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar filtros: {ex.Message}");
+            }
+        }
+
+        private async void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+
+                int? clienteId = null;
+                if (cmbCliente.SelectedValue != null && (int)cmbCliente.SelectedValue > 0)
+                {
+                    clienteId = (int)cmbCliente.SelectedValue;
+                }
+
+                int? statusId = null;
+                if (cmbStatusVenda.SelectedValue != null && (int)cmbStatusVenda.SelectedValue >= 0)
+                {
+                    statusId = (int)cmbStatusVenda.SelectedValue;
+                }
+
+                bool filtrarPorVenda = rdbDataVenda.Checked;
+
+                var dados = await _vendaRepo.ObterDadosRelatorioAsync(
+                    clienteId,
+                    statusId,
+                    dtInicial.Value, 
+                    dtFinal.Value,   
+                    filtrarPorVenda
+                );
+
+                if (!dados.Any())
+                {
+                    MessageBox.Show("Nenhum registro encontrado para os filtros selecionados.");
+                    reportViewer1.LocalReport.DataSources.Clear();
+                    reportViewer1.RefreshReport();
+                    return;
+                }
+
+                reportViewer1.LocalReport.DataSources.Clear();
+                reportViewer1.LocalReport.ReportPath = "Relatorios\\VendaResumido.rdlc";
+
+                var rds = new ReportDataSource("dsVendasGeral", dados);
+
+                reportViewer1.LocalReport.DataSources.Add(rds);
+                reportViewer1.RefreshReport();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao gerar relatório: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
         }
     }
 }
